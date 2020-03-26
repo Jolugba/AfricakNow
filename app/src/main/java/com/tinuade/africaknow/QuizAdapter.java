ef5b09af30372a19af10af3befe23fe8b395b052
missTinuade <estherjolugba@yahoo.com>
2020-03-26 10:17:47 +0000

    my Progress so far

--- app/src/main/java/com/tinuade/africaknow/QuizAdapter.java
+++ app/src/main/java/com/tinuade/africaknow/QuizAdapter.java
@@ -12,11 +12,7 @@ import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
-import androidx.recyclerview.widget.RecyclerView;
-
-/**
- * Created by SegunFrancis
- */
+import androidx.recyclerview.widget.RecyclerVi
 public class QuizAdapter extends RecyclerView.Adapter<QuizAdapter.QuizViewHolder> {
 
     private List<Question> mQuestionList;
