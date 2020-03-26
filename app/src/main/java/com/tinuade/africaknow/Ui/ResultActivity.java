d9397b4e4919bcfe536dfc1a3f46bf56f7eb990a
missTinuade <estherjolugba@yahoo.com>
2020-03-26 18:49:46 +0000

    AFRICA kNow DEMO final commit

--- app/src/main/java/com/tinuade/africaknow/Ui/ResultActivity.java
+++ app/src/main/java/com/tinuade/africaknow/Ui/ResultActivity.java
@@ -15,6 +15,7 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
         Intent intent = getIntent();
 
         String text = intent.getStringExtra(QuestionActivity.EXTRA_TEXT);
