ea9e7e5abbd5f5e1fdcdcef835745402c7c69eee
missTinuade <estherjolugba@yahoo.com>
2020-03-27 03:32:40 +0000

    AFRICA kNow DEMO final commit

--- app/src/main/java/com/tinuade/africaknow/Ui/ResultActivity.java
+++ app/src/main/java/com/tinuade/africaknow/Ui/ResultActivity.java
@@ -9,7 +9,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
